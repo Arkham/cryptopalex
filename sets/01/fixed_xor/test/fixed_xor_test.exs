8eafb72677116aa4fa29edaae5b45d9b4e1fcbfa
SamGerber <gerbyxvi@gmail.com>
2017-10-18 16:40:58 -0700

    Test the cryptopals expectation

--- sets/01/fixed_xor/test/fixed_xor_test.exs
+++ sets/01/fixed_xor/test/fixed_xor_test.exs
@@ -5,4 +5,10 @@ defmodule FixedXorTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+  test "" do
+    string_one = "1c0111001f010100061a024b53535009181c"
+    string_two = "686974207468652062756c6c277320657965"
+    result = "746865206b696420646f6e277420706c6179"
+    assert FixedXor.fixed_xor(string_one, string_two) == result
+  end
 end
