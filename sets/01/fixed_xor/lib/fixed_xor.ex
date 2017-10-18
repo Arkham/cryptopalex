857265c31c64dd753a7f33a2b3d9754850fb6e42
SamGerber <gerbyxvi@gmail.com>
2017-10-18 16:37:10 -0700

    initial commit for fixed_xor

---
+++ sets/01/fixed_xor/lib/fixed_xor.ex
@@ -0,0 +1,18 @@
+defmodule FixedXor do
+  @moduledoc """
+  Documentation for FixedXor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FixedXor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ sets/01/fixed_xor/test/fixed_xor_test.exs
@@ -0,0 +1,8 @@
+defmodule FixedXorTest do
+  use ExUnit.Case
+  doctest FixedXor
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
